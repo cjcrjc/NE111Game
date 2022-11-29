74f589a4a15f93fe4e860c99124d8a68a650caee
Sam Krysko <sdkrysko@uwaterloo.ca>
2022-11-28 20:59:23 -0500

    got rid tileset files to retry map creation

--- Decor 16x16 Tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Decor 16x16 Tiles" tilewidth="16" tileheight="16" tilecount="20" columns="4">
- <image source="../../../../../Users/samkr/Downloads/mystic_woods_free_v0.2/sprites/tilesets/decor_16x16.png" width="64" height="80"/>
-</tileset>
--- Plains Tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Plains Tiles" tilewidth="16" tileheight="16" tilecount="72" columns="6">
- <image source="../../../../../Users/samkr/Downloads/mystic_woods_free_v0.2/sprites/tilesets/plains.png" width="96" height="192"/>
-</tileset>
